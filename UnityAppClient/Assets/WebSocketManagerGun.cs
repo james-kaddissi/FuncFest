0f39ac4bc28a428278e2c25460e726b55b803187
ANONyMOSy <jameskaddissi@gmail.com>
2024-11-12 11:30:59 -0500

    Shooting controls

--- UnityAppClient/Assets/WebSocketManagerGun.cs
+++ UnityAppClient/Assets/WebSocketManagerGun.cs
@@ -7,39 +7,18 @@ public class WebSocketManagerGun : MonoBehaviour
 {
     private WebSocketRouter webSocketRouter;
     [SerializeField] private InputActionReference moveActionToUse;
-
-    private float fireCooldown = 5f;
-    private float lastFireTime; 
-    [SerializeField] private TextMeshProUGUI fireText;
+    [SerializeField] private InputActionReference shootActionToUse;
     
     void Start() {
         webSocketRouter = GameObject.Find("WebSocketRouter").GetComponent<WebSocketRouter>();
         if (webSocketRouter == null) {
             Debug.LogWarning("WebSocketRouter not found in scene. Ensure it is present.");
         }
-        lastFireTime = -5f;
     }
 
     void Update() {
         
-        Vector2 moveDirection=moveActionToUse.action.ReadValue<Vector2>();
-        webSocketRouter.SendInput($"move {moveDirection.x} {moveDirection.y}");
-
-        if (Time.time < lastFireTime + fireCooldown) {
-            float remainingTime = lastFireTime + fireCooldown - Time.time;
-            fireText.text = $"{Math.Ceiling(remainingTime)}";
-        } else {
-            fireText.text = "FIRE!";
-        }
-    }
-
-    public void FireControl()
-    {
-        if (Time.time >= lastFireTime + fireCooldown) {
-            webSocketRouter.SendInput("fire");
-            lastFireTime = Time.time; 
-        } else {
-            Debug.Log("Fire is on cooldown.");
-        }
+        Vector2 shootDirection=moveActionToUse.action.ReadValue<Vector2>();
+        webSocketRouter.SendInput($"shoot {shootDirection.x} {shootDirection.y}");
     }
 }
